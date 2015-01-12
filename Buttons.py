b4b536b65ebb4a7b8cf6587ab295c57f5f4e80d6
jordan139 <jordan139@hotmail.co.uk>
2015-01-12 21:26:59 +0000

    Button created, need to add in overlay
    Button is currently functioning , need to create the overlay in
    photoshop and then add in functionality.

--- Buttons.py
+++ Buttons.py
@@ -3,46 +3,68 @@ from pygame.locals import *
 import sys
 
 
-class Button():
+class Button(pygame.Surface):
 
     def __init__(self):
 
-        ##load photoshoped image of button 
-        self.image = pygame.image.load('Button_Close.png')
-        ##create the font for button 
-        self.myfont = pygame.font.SysFont("Lucida Sans Unicode", 20)
-        ##create text, colour 
-        self.label = self.myfont.render("CLOSE", 1, (255,255,255))
+        pygame.Surface.__init__(self, size = (100,50))
+        self.hovered = False
 
-    def buttonFont(self,screen): 
-       self.image.blit(self.label, (30,5))
-       screen.blit(self.image, (0,0))
+        button = pygame.image.load("buttoncloseNew.png")
+        self.state_normal = button.subsurface((0,0,100,25))
+        self.state_hover = button.subsurface((0,25,100,25))
+        self.myfont = pygame.font.SysFont("Lucida Sans Unicode", 20) 
+        self.label = self.myfont.render("CLOSE", 1, (142,142,142))
 
-       image = pygame.Surface([100,25], pygame.SRCALPHA, 32)
-       image = image.convert_alpha()
+    def update(self,screen):
+        self.process_events()
+        self.fill((0,0,0))
 
+        newimage = self.get_image()
+        self.blit(newimage, newimage.get_rect())
+        self.blit(self.label, (30,5))
+
+        screen.blit(self, self.get_rect())
+        
     def buttonMotion():
         if event.type == pygame.MOUSEMOTION:
                 print "This will close the program"
+
+    def get_image(self):
+        if self.hovered:
+            return self.state_hover
+        else:
+            return self.state_normal
+
+    def process_events(self):
+        for event in pygame.event.get():
+            if event.type == pygame.MOUSEMOTION:
+                if self.get_rect().collidepoint(event.pos):
+                        self.hovered = True         
+                else:   
+                    self.hovered = False
+                    if event.type == QUIT:
+                        pygame.quit()
+                        sys.exit 
        
                                          
 pygame.init()
 
-screen = pygame.display.set_mode((100, 25))
+screen = pygame.display.set_mode((200, 200))
  
 pygame.display.set_caption('Close Button')
 
 button = Button()
+clock = pygame.time.Clock()
 
 while True :
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-
-    
-    button.buttonFont(screen)
+    button.update(screen)
     pygame.display.update()
+    clock.tick(30)
+                                              
+
+
+
 
 
             
